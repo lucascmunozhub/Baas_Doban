371878b7534a95aa5cd8277ab9a10f9b203c496d
lucascmunozhub <lucascmunoz@gmail.com>
2025-03-13 15:34:49 -0300

    v7

--- app/main.py
+++ app/main.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from app.handshake import router as handshake_router
 from app.callback import router as callback_router
+from app.teste import router as teste_router 
 
 app = FastAPI(title="Minha API de Integração")
 
 # Incluindo as rotas
 app.include_router(handshake_router, prefix="/handshake", tags=["Handshake"])
 app.include_router(callback_router, prefix="/callback", tags=["Callback"])
+app.include_router(teste_router, prefix="/teste", tags=["Teste"])
 
 @app.get("/")
 async def root():
